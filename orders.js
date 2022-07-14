d5571f79a442881aa4dd0f5453441a354d0d3813
Guigan713 <guillaume.lequin713@gmail.com>
2022-07-14 22:07:12 +0200

    second commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Nike Air Max 1 Patta Monarch',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Asics Gel Lyte 3 Salmon Toe',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'New Balance 990v3 JJJJound',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Adidas Futurecraft 4d',
+        productNumber: '96996',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'Asics GT-2 BBB Salmon',
+        productNumber: '22821',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'Asics Gel Lyte 3 Blue Ribbon Jcrew',
+        productNumber: '22821',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+];
